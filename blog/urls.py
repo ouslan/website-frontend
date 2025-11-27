005b7ff6d5baf9abc457b7a68e2131b681975c77
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-11-27 18:10:40 -0400

    added working blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.blog_index, name="index"),  # Blog index view
+    path("", views.blog_index, name="blog_index"),
+    path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("category/<category>/", views.blog_category, name="blog_category"),
 ]
